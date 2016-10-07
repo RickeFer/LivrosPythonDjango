ac73a4f253e06a12cfabf74d0a51da2d4c22819a
Henrique Ferreira <henrique.fer@hotmail.com>
2016-10-06 23:28:01 -0300

    Add files via upload

--- livros/settings.py
+++ livros/settings.py
@@ -100,13 +100,16 @@ import urlparse
 
 urlparse.users_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
+u = 'postgres://ycauwrblzglhuz:c-4KBTuGBF1Flr1W2q-dNlFsmQ@ec2-54-235-125-172.compute-1.amazonaws.com:5432/ddtatfrbd2t1ij'
 conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
+    database='ddtatfrbd2t1ij',
+    user='ycauwrblzglhuz',
+    password='c-4KBTuGBF1Flr1W2q-dNlFsmQ',
+    host=u+'ec2-54-235-125-172.compute-1.amazonaws.com',
+    port=5432
 )
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 """
 DATABASES = {
     'default': {
